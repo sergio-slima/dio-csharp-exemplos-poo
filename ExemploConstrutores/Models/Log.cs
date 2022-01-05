da08d33a7a82a2ae181b7e361959fc5083aec675
sergio-slima <sergio_silvalima@hotmail.com>
2022-01-05 06:10:55 -0300

    Propriedades Get e Set

--- ExemploConstrutores/Models/Log.cs
+++ ExemploConstrutores/Models/Log.cs
@@ -1,23 +1,23 @@
 namespace ExemploConstrutores.Models
 {
-    public class Log
+  public class Log
+  {
+    private static Log? _log;
+
+    public string? PropriedadeLog { get; set; }
+
+    // private Log()
+    // {
+
+    // }
+
+    public static Log GetInstance()
     {
-        private static Log _log;
-
-        public string PropriedadeLog { get; set; }
-
-        private Log()
-        {
-
-        }
-
-        public static Log GetInstance()
-        {
-            if (_log == null)
-            {
-                _log = new Log();
-            }
-            return _log;
-        }
+      if (_log == null)
+      {
+        _log = new Log();
+      }
+      return _log;
     }
+  }
 }
--- ExemploConstrutores/Program.cs
+++ ExemploConstrutores/Program.cs
@@ -7,14 +7,20 @@ namespace ExemploConstrutores
   {
     static void Main(string[] args)
     {
+      Data data = new Data();
+      data.setMes(5);
+      data.Mes = 13;
+
+      data.ApresentarMes();
+
       // Log log = Log.GetInstance();
       // log.PropriedadeLog = "Teste Instancia";
 
       // Log log2 = Log.GetInstance();
       // System.Console.WriteLine(log2.PropriedadeLog);
 
-      Aluno p1 = new Aluno("Sergio", "Lima", "Matematica");
-      p1.Apresentar();
+      // Aluno p1 = new Aluno("Sergio", "Lima", "Matematica");
+      // p1.Apresentar();
     }
   }
 }
