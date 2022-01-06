fc46ff30473c247cec6dfb95928a771f2dcad62f
sergio-slima <sergio_silvalima@hotmail.com>
2022-01-05 22:35:56 -0300

    Csharp Modificadores e Delegates

--- ExemploConstrutores/Models/Pessoa.cs
+++ ExemploConstrutores/Models/Pessoa.cs
@@ -1,26 +1,35 @@
 namespace ExemploConstrutores.Models
 {
-    public class Pessoa
+  public class Pessoa
+  {
+    private readonly string nome;
+    private string sobrenome;
+
+    public Pessoa()
     {
-        private string nome;
-        private string sobrenome;
-
-        // public Pessoa()
-        // {
-        //     nome = string.Empty;
-        //     sobrenome = string.Empty;
-        // }
-
-        public Pessoa(string nome, string sobrenome)
-        {
-            this.nome = nome;
-            this.sobrenome = sobrenome;
-            System.Console.WriteLine("Construtor classe Pessoa");
-        }
-
-        public void Apresentar()
-        {
-            System.Console.WriteLine($"Olá, meu nome é: {nome} {sobrenome}");
-        }
+      nome = string.Empty;
+      sobrenome = string.Empty;
     }
+
+    public Pessoa(string nome, string sobrenome)
+    {
+      this.nome = nome;
+      this.sobrenome = sobrenome;
+      System.Console.WriteLine("Construtor classe Pessoa");
+    }
+
+    public string Sobrenome { get => sobrenome; set => sobrenome = value; }
+    public string Sobrenome1 { get => sobrenome; set => sobrenome = value; }
+
+    public void Apresentar()
+    {
+      System.Console.WriteLine($"Olá, meu nome é: {nome} {sobrenome}");
+    }
+
+    public override bool Equals(object? obj)
+    {
+      return obj is Pessoa pessoa &&
+             sobrenome == pessoa.sobrenome;
+    }
+  }
 }
--- ExemploConstrutores/Program.cs
+++ ExemploConstrutores/Program.cs
@@ -5,20 +5,36 @@ namespace ExemploConstrutores
 {
   class Program
   {
+    public delegate void Operacao(int x, int y);
     static void Main(string[] args)
     {
-      Data data = new Data();
-      data.setMes(5);
-      data.Mes = 13;
+      Matematica m = new Matematica(10, 20);
+      m.Somar();
 
-      data.ApresentarMes();
+      // CLASS CALCULADORA - DELEGATE
+      // Operacao op = new Operacao(Calculadora.Somar);
+      // op += Calculadora.Subtrair;
+      // op.Invoke(10, 5);
 
+      // MODIFICADOR CONSTANTE
+      // const double pi = 3.14;
+      // System.Console.WriteLine(pi);
+
+      // CLASSE DATA
+      // Data data = new Data();
+      // data.setMes(5);
+      // data.Mes = 13;
+
+      // data.ApresentarMes();
+
+      // CLASSE PESSOA
       // Log log = Log.GetInstance();
       // log.PropriedadeLog = "Teste Instancia";
 
       // Log log2 = Log.GetInstance();
       // System.Console.WriteLine(log2.PropriedadeLog);
 
+      // CLASSE ALUNO
       // Aluno p1 = new Aluno("Sergio", "Lima", "Matematica");
       // p1.Apresentar();
     }
