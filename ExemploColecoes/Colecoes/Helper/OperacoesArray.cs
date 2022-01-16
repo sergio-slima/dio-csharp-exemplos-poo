17b7860572e08730306bce5679ec466f1df15290
sergio-slima <sergio_silvalima@hotmail.com>
2022-01-16 10:33:29 -0300

    Ordenando com Bubble Sort

---
+++ ExemploColecoes/Colecoes/Helper/OperacoesArray.cs
@@ -0,0 +1,28 @@
+namespace Colecoes.Helper
+{
+  public class OperacoesArray
+  {
+    public void OrdernarBubbleSort(ref int[] array)
+    {
+      int temp = 0;
+
+      for (int i = 0; i < array.Length; i++)
+      {
+        for (int j = 0; j < array.Length - 1; j++)
+        {
+          if (array[j] > array[j + 1])
+          {
+            temp = array[j + 1];
+            array[j + 1] = array[j];
+            array[j] = temp;
+          }
+        }
+      }
+    }
+    public void ImprimirArray(int[] array)
+    {
+      var linha = string.Join(", ", array);
+      System.Console.WriteLine(linha);
+    }
+  }
+}
--- ExemploColecoes/Colecoes/Program.cs
+++ ExemploColecoes/Colecoes/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using Colecoes.Helper;
 
 namespace Colecoes
 {
@@ -7,22 +8,35 @@ namespace Colecoes
   {
     static void Main(string[] args)
     {
-      int[,] matriz = new int[4, 2]
-      {
-        {8,8},
-        {10,20},
-        {50,100},
-        {90,200}
-      };
+      // ARRAY BUBLLE SORT
+      OperacoesArray op = new OperacoesArray();
+      int[] array = new int[5] { 6, 3, 8, 1, 9 };
 
-      for (int i = 0; i < matriz.GetLength(0); i++)
-      {
-        for (int j = 0; j < matriz.GetLength(1); j++)
-        {
-          System.Console.WriteLine(matriz[i, j]);
-        }
-      }
+      System.Console.WriteLine("Array original:");
+      op.ImprimirArray(array);
 
+      op.OrdernarBubbleSort(ref array);
+      System.Console.WriteLine("Array ordenado:");
+      op.ImprimirArray(array);
+
+      // ARRAY MATRIZ
+      // int[,] matriz = new int[4, 2]
+      // {
+      //   {8,8},
+      //   {10,20},
+      //   {50,100},
+      //   {90,200}
+      // };
+
+      // for (int i = 0; i < matriz.GetLength(0); i++)
+      // {
+      //   for (int j = 0; j < matriz.GetLength(1); j++)
+      //   {
+      //     System.Console.WriteLine(matriz[i, j]);
+      //   }
+      // }
+
+      // ARRAY INT
       // int[] arrayInteiros = new int[3];
 
       // arrayInteiros[0] = 10;
